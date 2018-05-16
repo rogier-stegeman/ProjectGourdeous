b1cf8439636bb040fdd1b8406132b8a9a8daeb07
RogierStegeman <rogier.stegeman@upcmail.nl>
2018-05-16 12:50:04 +0200

    all sunburst stuff adde

--- Gourdeous/app.py
+++ Gourdeous/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,6 +6,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!!!!'
+
+
+@app.route('/sunburst')
+def sunburst():
+    return render_template("Sunburst.html")
 
 
 if __name__ == '__main__':
