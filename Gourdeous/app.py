b64a1a21cf140ac3e279c1a1123beff6ec6b7419
Bram <bramgelsink@gmail.com>
2018-06-04 20:33:10 +0200

    Afronding applicatie. hopelijk

--- Gourdeous/app.py
+++ Gourdeous/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, url_for, request, redirect, flash
 from Bio import Entrez
 import mysql.connector
 from Bio import Medline
+import Gourdeous_textminer
 
 app = Flask(__name__)
 #app = Flask(__name__)
@@ -19,29 +20,17 @@ def home():
 
 @app.route('/textmine')
 def textmine():
-    organisms = ['bitter gourd', 'yam', 'Momordica charantia', 'Dioscorea batatas']
-    compounds = ['fatty acid', 'insulin', 'cholesterol', 'sugar', 'glucose', 'vitamin A', 'vitamin B', 'vitamin C',
-                 'vitamin E', 'calcium', 'iron', 'potassium']
-    health_effects = ['diabetes', 'cancer', 'weigth loss', 'cough', 'wounds', 'rheumatism', 'laxative', 'diarrhea',
-                      'abdominal pain', 'fever', 'hypoglycemia', 'urinary incontinence', 'chest pain', 'miscarriage']
-    return render_template('textmine.html', organisms=organisms, compounds=compounds, health_effects=health_effects)
+    return render_template('textmine.html')
 
-@app.route('/busy', methods=['GET','POST'])
+@app.route('/Done', methods=['GET','POST'])
 def submitter():
-    #return render_template("sorry.html")
-    #return render_template("Busy.html")
     #This function is meant to allow the user to add data in the form of search requests to the database
     # in order to expand the database.
-    organisme = request.form['search1']
-    zoekwoord = request.form['search2']
-    email = request.form['search3']
-    zoekwoord = "diarrhea"
-    organisme = "dioscorea batatas"
-    conn = mysql.connector.connect(user='owe8_pg9', password='blaat1234',
-                                    host="localhost",
-                                    database='owe8_pg9')
-    conn.close() #code voor textmining invoegen of verander render template in sorry.html.
-    return render_template("Busy.html")
+    organisme = request.form['searchPlant']
+    zoekwoord = request.form['searchHealth']
+    email = request.form['searchMail']
+    Gourdeous_textminer.main(organisme,zoekwoord,email)
+    return render_template("Done.html")
 
 
 @app.route('/sunburst')
