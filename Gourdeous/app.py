b9362cce200d130b445ed64fbc172eef7a0d5c13
Bram <bramgelsink@gmail.com>
2018-06-06 12:44:59 +0200

    save file

--- Gourdeous/app.py
+++ Gourdeous/app.py
@@ -1,8 +1,5 @@
-from flask import Flask, render_template, url_for, request, redirect, flash
-from Bio import Entrez
-import mysql.connector
-from Bio import Medline
-import Gourdeous_textminer
+from flask import Flask, render_template, url_for, request, redirect
+#import Gourdeous_textminer
 
 app = Flask(__name__)
 
@@ -27,7 +24,7 @@ def submitter():
     organisme = request.form['searchPlant']
     zoekwoord = request.form['searchHealth']
     email = request.form['searchMail']
-    Gourdeous_textminer.main(organisme,zoekwoord,email)
+   # Gourdeous_textminer.main(organisme, zoekwoord, email)
     return render_template("Done.html")
 
 
