7c6367d558897f0c39555bb311c8cc45085d3987
Bram <bramgelsink@gmail.com>
2018-06-06 19:34:48 +0200

    commentaar toegevoegd

--- Gourdeous/app.py
+++ Gourdeous/app.py
@@ -1,63 +1,57 @@
 from flask import Flask, render_template, url_for, request, redirect
 from  Gourdeous_textminer import connector,entrez_search,db_vullen
-import time
-from Bio import Entrez
-<<<<<<< HEAD
-from Bio import Medline
-import urllib2
-import mysql.connector
-from flask import Flask
-import MySQLdb as my
-import sys
-=======
+#import time
+#from Bio import Entrez
+#from Bio import Medline
+#import urllib2
 #import mysql.connector
-from Bio import Medline
+from flask import Flask
+#import MySQLdb as my
+#import sys
 
->>>>>>> bdb52c3a11900d3732ec81e45b2024ff0f4aa78b
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return redirect(url_for('home'))
+    return redirect(url_for('home')) #deze functie verwijst door naar de home pagina
 
 
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html') # deze functie verwijst naar de home pagina
 
 
 @app.route('/textmine')
 def textmine():
-    return render_template('textmine.html')
+    return render_template('textmine.html') # deze functie verwijst naar de textminer pagina
 
-@app.route('/Done', methods=['GET','POST'])
+@app.route('/Done', methods=['GET','POST']) # deze functie verwijst naar de done pagina en haalt informatie op van de textmine pagina
+# die gebruikt kan worden door de textmine script en door pubmed
 def submitter():
     #This function is meant to allow the user to add data in the form of search requests to the database
     # in order to expand the database.
     organisme = request.form['searchPlant']
     zoekwoord = request.form['searchHealth']
     email = request.form['searchMail']
-    #data = entrez_search(organisme,zoekwoord,email)
-    #con = connector()
-    #return data
-   # db_vullen(data, organisme, zoekwoord, con)
-    #Gourdeous_textminer.main(organisme, zoekwoord, email)
+    data = entrez_search(organisme,zoekwoord,email)
+    con = connector()
+    db_vullen(data, organisme, zoekwoord, con)
+    return render_template("Done.html")
 
 
 
-
-@app.route('/sunburst')
+@app.route('/sunburst') # deze functie verwijst naar de sunburst pagina
 def sunburst():
     return render_template("sunburst.html")
 
 
-@app.route('/help')
+@app.route('/help') # Deze functie verwijst naar de help pagina
 def helppage():
     return render_template("help.html")
 
 
-@app.route('/cook')
+@app.route('/cook') # deze functie verwijst naar de cooking pagina
 def cook():
     return render_template("cook.html")
 
